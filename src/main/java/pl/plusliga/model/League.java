5465f7f0c7ca348341f5a6965be526f35cb4805f
Piotr Melkowski <piotr.melkowski@gmail.com>
2019-02-17 19:57:39 +0100

    Update LSK games URL.

--- src/main/java/pl/plusliga/model/League.java
+++ src/main/java/pl/plusliga/model/League.java
@@ -7,7 +7,7 @@ public enum League {
 
   ORLENLIGA("http://www.lsk.plps.pl/teams.html",
       "http://www.lsk.plps.pl/players.html?memo=%7B%22players%22%3A%7B%22mainFilter%22%3A%22letter%22%2C%22subFilter%22%3A%22all%22%7D%7D",
-      "http://www.lsk.plps.pl/games.html?memo=%7B%22games%22%3A%7B%7D%7D",
+      "http://www.lsk.pls.pl/games.html?memo=%7B%22games%22%3A%7B%22faza%22%3A1%2C%22runda%22%3A1%7D%7D",
       "http://www.lsk.pls.pl/pcup/s/games.html",
       "http://www.lsk.pls.pl/scup/s/game.html",
       Position.atakująca, Position.libero, Position.przyjmująca, Position.rozgrywająca,
