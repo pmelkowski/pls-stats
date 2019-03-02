10df9613686e24367a8c43bc8780d9f32d23064f
Piotr Melkowski <piotr.melkowski@gmail.com>
2019-03-02 10:57:37 +0100

    Fix reading players' start and end date.

--- src/main/java/pl/plusliga/parser/pls/PlpsPlayerTeamParser.java
+++ src/main/java/pl/plusliga/parser/pls/PlpsPlayerTeamParser.java
@@ -3,6 +3,7 @@ package pl.plusliga.parser.pls;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Optional;
 import java.util.regex.Pattern;
 import org.jsoup.nodes.Element;
 import pl.plusliga.model.Player;
@@ -24,10 +25,8 @@ public class PlpsPlayerTeamParser implements JsoupParser<PlayerTeam> {
 
   @Override
   public PlayerTeam getEntity(Element element) {
-    Date dateFrom = getDate(element.child(0).text(), DATE_FORMAT)
-        .filter(date -> date.after(START_DATE))
-        .orElse(null);
-    if (dateFrom == null) {
+    Optional<Date> dateTo = getDate(element.child(1).text(), DATE_FORMAT);
+    if (dateTo.filter(date -> date.before(START_DATE)).isPresent()) {
       return null;
     }
 
@@ -35,19 +34,12 @@ public class PlpsPlayerTeamParser implements JsoupParser<PlayerTeam> {
     key.setPlayerId(player.getId());
     getInteger(element.child(4).getElementsByTag("a").first().absUrl("href"), TEAM_ID_PATTERN, 1)
         .ifPresent(key::setTeamId);
-    key.setDateFrom(dateFrom);
+    getDate(element.child(0).text(), DATE_FORMAT).ifPresent(key::setDateFrom);
 
     PlayerTeam team = new PlayerTeam();
     team.setKey(key);
-    getDate(element.child(1).text(), DATE_FORMAT).ifPresent(key::setDateFrom);
-
-    String position = element.child(2).text().toLowerCase();
-    if (player.getName().equals("Cutura Hana") || position.isEmpty()) {
-      System.err.println(player.getName() + " -> przyjmująca");
-      team.setPosition(Position.przyjmująca);
-    } else {
-      team.setPosition(Position.valueOf(position));
-    }
+    dateTo.ifPresent(team::setDateTo);
+    team.setPosition(Position.valueOf(element.child(2).text().toLowerCase()));
 
     return team;
   }
